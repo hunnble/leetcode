b1d0f06c1c9058666e84f76683ab9d84733a703d
hunnble <hante_hunter@163.com>
2018-01-25 11:09:19 +0800

    645

---
+++ 645.SetMismatch.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int[] result = new int[2];
+        for (int i : nums) {
+            if (nums[Math.abs(i) - 1] < 0) {
+                result[0] = Math.abs(i);
+            } else {
+                nums[Math.abs(i) - 1] *= -1;
+            }
+        }
+        for (int i = 0; i < nums.length; i += 1) {
+            if (nums[i] > 0) {
+                result[1] = i + 1;
+            }
+        }
+        return result;
+    }
+}
