c1148e495c39f1943435d45f1d056ed0872fe25f
hunnble <hante_hunter@163.com>
2018-02-02 20:40:47 +0800

    637

---
+++ 637.AverageofLevelsinBinaryTree.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new ArrayList<Double>();
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        if (root == null) {
+            return result;
+        }
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            double sum = 0.0;
+            for(int i = 0; i < n; i++) {
+                TreeNode node = queue.poll();
+                sum += node.val;
+                if(node.left != null) {
+                    queue.offer(node.left);
+                }
+                if(node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            result.add(sum / n);
+        }
+        return result;
+    }
+}
