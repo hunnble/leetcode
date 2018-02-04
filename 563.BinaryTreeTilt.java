d13b75959b433a48787936e5deeee97d06a3f2ec
hunnble <hante_hunter@163.com>
2018-02-04 18:58:06 +0800

    563

---
+++ 563.BinaryTreeTilt.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int result = 0;
+
+    public int findTilt(TreeNode root) {
+        helper(root);
+        return result;
+    }
+
+    private int helper(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = helper(root.left);
+        int right = helper(root.right);
+        result = result + Math.abs(left - right);
+        return left + right + root.val;
+    }
+}
