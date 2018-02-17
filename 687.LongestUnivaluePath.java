ab6e40535c14172ae5fd554494b164bfff177280
hunnble <hante_hunter@163.com>
2018-02-17 13:52:04 +0800

    687

---
+++ 687.LongestUnivaluePath.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    private int len = 0;
+
+    public int longestUnivaluePath(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        getLen(root, root.val);
+        return len;
+    }
+
+    private int getLen(TreeNode node, int val) {
+        if (node == null) {
+            return 0;
+        }
+        int left = getLen(node.left, node.val);
+        int right = getLen(node.right, node.val);
+        len = Math.max(len, left + right);
+        if (node.val == val) {
+            return Math.max(left, right) + 1;
+        }
+        return 0;
+    }
+}
