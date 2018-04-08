2b4fe14e752b717196b83294ffef0eb0324c2487
hunnble <hante_hunter@163.com>
2018-04-08 17:44:42 +0800

    307(segment tree method)

--- 307.RangeSumQuery-Mutable.java
+++ 307.RangeSumQuery-Mutable.java
@@ -45,6 +45,83 @@ class NumArray {
 }
 
 /**
+ * segment tree
+ */
+class NumArray {
+    class SegmentTreeNode {
+        int start, end;
+        SegmentTreeNode left, right;
+        int sum;
+
+        public SegmentTreeNode(int start, int end) {
+            this.start = start;
+            this.end = end;
+            this.left = null;
+            this.right = null;
+            this.sum = 0;
+        }
+    }
+
+    SegmentTreeNode root = null;
+
+    private SegmentTreeNode buildTree(int[] nums, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+        SegmentTreeNode node = new SegmentTreeNode(start, end);
+        if (start == end) {
+            node.sum = nums[start];
+        } else {
+            int mid = start + (end - start) / 2;
+            node.left = buildTree(nums, start, mid);
+            node.right = buildTree(nums, mid + 1, end);
+            node.sum = node.left.sum + node.right.sum;
+        }
+        return node;
+    }
+
+    private void update(SegmentTreeNode node, int i, int val) {
+        if (node.start == node.end) {
+            node.sum = val;
+        } else {
+            int mid = node.start + (node.end - node.start) / 2;
+            if (i <= mid) {
+                update(node.left, i, val);
+            } else {
+                update(node.right, i, val);
+            }
+            node.sum = node.left.sum + node.right.sum;
+        }
+    }
+
+    private int sumRange(SegmentTreeNode node, int start, int end) {
+        if (node.start == start && node.end == end) {
+            return node.sum;
+        }
+        int mid = node.start + (node.end - node.start) / 2;
+        if (end <= mid) {
+            return sumRange(node.left, start, end);
+        } else if (start >= mid + 1) {
+            return sumRange(node.right, start, end);
+        } else {
+            return sumRange(node.left, start, mid) + sumRange(node.right, mid + 1, end);
+        }
+    }
+
+    public NumArray(int[] nums) {
+        root = buildTree(nums, 0, nums.length - 1);
+    }
+
+    public void update(int i, int val) {
+        update(root, i, val);
+    }
+
+    public int sumRange(int i, int j) {
+        return sumRange(root, i, j);
+    }
+}
+
+/**
  * Your NumArray object will be instantiated and called as such:
  * NumArray obj = new NumArray(nums);
  * obj.update(i,val);
