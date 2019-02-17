6ad2733aedf67522f7be3c8fc203f62a70af03c2
hunnble <hante_hunter@163.com>
2019-02-17 13:21:17 +0800

    190, bit manipulation

--- 190_ReverseBits.py
+++ 190_ReverseBits.py
@@ -5,3 +5,13 @@ class Solution:
         oribin = '{0:032b}'.format(n)
         reversebin = oribin[::-1]
         return int(reversebin, 2)
+
+class Solution1:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+        res = 0
+        for i in range(32):
+            res = (res << 1) + (n & 1)
+            n >>= 1
+        return res
