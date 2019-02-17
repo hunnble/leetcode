34ace6a8c0d8470232d5cfac664d56d7a8c8fa0e
hunnble <hante_hunter@163.com>
2019-02-18 01:03:33 +0800

    46, optimization

--- 46_Permutations.py
+++ 46_Permutations.py
@@ -1,11 +1,8 @@
 class Solution(object):
     def pushChar(self, nums, curNums, res):
         for i in range(len(nums)):
-            tmpNums = curNums[:]
-            num = nums[i]
-            tmpNums.append(num)
-            nextNums = nums[:]
-            nextNums.pop(i)
+            tmpNums = curNums[:] + [nums[i]]
+            nextNums = nums[0:i] + nums[i + 1:]
             if not nextNums:
                 res.append(tmpNums)
             else:
